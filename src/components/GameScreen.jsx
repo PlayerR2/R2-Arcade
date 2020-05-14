510430b665f41aafbdf28bad75534654c5f55a7b
Riku Kawano <rrrikukawano@gmail.com>
2020-05-14 12:40:54 +0900

    Created login/register modals

--- src/components/GameScreen.jsx
+++ src/components/GameScreen.jsx
@@ -1,19 +1,14 @@
 import { Modal, Button } from "react-bootstrap";
-import Iframe from "react-iframe";
 
 export default function GameScreen(props) {
   return (
     <Modal {...props} size="lg" centered dialogClassName="modal-90w">
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">Game Title</Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">
+          GAME TITLE HERE
+        </Modal.Title>
       </Modal.Header>
-      <Modal.Body>
-        <Iframe
-          url="https://i.simmer.io/@finkagain/bally"
-          width="900px"
-          height="540px"
-        ></Iframe>
-      </Modal.Body>
+      <Modal.Body>GAME RENDERED HERE</Modal.Body>
       <Modal.Footer>
         <Button onClick={props.onHide}>Close</Button>
       </Modal.Footer>
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,98 @@
-import { Navbar, Form, Button } from "react-bootstrap";
+import { Modal, Navbar, Form, Button, Col, Row } from "react-bootstrap";
+import React, { useState } from "react";
 
 export default function Navigation() {
+  const [modalShow, setModalShow] = React.useState(false);
+  const [modalType, setModalType] = React.useState("👋 Login");
+  const [modalBtn, setModalBtn] = React.useState("🔥 Enter  🔥");
+
   return (
     <>
       <Navbar bg="light" variant="light">
         <Navbar.Brand href="#home">👾Game Center</Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
           <Form inline>
-            <Button variant="outline-primary mr-sm-2">Login</Button>
-            <Button variant="outline-primary">Upload</Button>
+            <Button
+              variant="outline-primary mr-sm-2"
+              onClick={() => {
+                setModalShow(true);
+                setModalType("👋 Login");
+                setModalBtn("🔥 Enter  🔥");
+              }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="primary"
+              onClick={() => {
+                setModalShow(true);
+                setModalType("🙌 Register");
+                setModalBtn("🔥 Enter  🔥");
+              }}
+            >
+              Register
+            </Button>
+            {/* Show when user isLoggedIn */}
+            {/* <Button
+              variant="outline-primary mr-sm-2"
+              onClick={() => {
+                signout user;
+              }}
+            >
+              Logout
+            </Button>
+            <Button
+              variant="success"
+              type="submit"
+              onClick={() => { get file input;
+              }}
+            >
+              Upload
+            </Button> */}
           </Form>
         </Navbar.Collapse>
       </Navbar>
+
+      <Modal show={modalShow} onHide={() => setModalShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalType}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            {modalType === "🙌 Register" && (
+              <Form.Group as={Row} controlId="formGroupUsername">
+                <Form.Label column sm="1">
+                  🎮
+                </Form.Label>
+                <Col sm>
+                  <Form.Control type="username" placeholder="Your username" />
+                </Col>
+              </Form.Group>
+            )}
+            <Form.Group as={Row} controlId="formGroupEmail">
+              <Form.Label column sm="1">
+                ✉️
+              </Form.Label>
+              <Col sm>
+                <Form.Control type="email" placeholder="Your email" />
+              </Col>
+            </Form.Group>
+            <Form.Group as={Row} controlId="formGroupPassword">
+              <Form.Label column sm="1">
+                🗝
+              </Form.Label>
+              <Col sm>
+                <Form.Control type="password" placeholder="Your password" />
+              </Col>
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => setModalShow(false)} block>
+            {modalBtn}
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
