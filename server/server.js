a4ccffff4cddfe7b90c8f83ca6d500b2f0f0f860
Roger V <60841571+rogerrogerv@users.noreply.github.com>
2020-05-15 21:50:33 +0900

    Add hotreloading for webpack, possibly broke Bally game loading

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const server = express()
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
 const port = process.env.PORT || 3000
+const config = require('../webpack.config')
+const compiler = webpack(config)
+
+server.use(webpackDevMiddleware(compiler, {
+  publicPath: config.output.publicPath,
+}))
 
 server.use('/', express.static('./public', {index: "index.html"}))
 
